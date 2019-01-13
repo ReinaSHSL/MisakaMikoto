6aeef2f7f489e09d17932896d16f98f727c7d3d8
ReinaSHSL <ReinaSHSL@users.noreply.github.com>
2019-01-12 19:58:39 -0800

    Negatively charged

---
+++ src/main/java/MisakaCode/powers/NegativelyChargedPower.java
@@ -0,0 +1,24 @@
+package MisakaCode.powers;
+
+import MisakaCode.tools.TextureLoader;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.powers.AbstractPower;
+
+public class NegativelyChargedPower extends AbstractPower {
+    public static final String POWER_ID = "misaka:Negative";
+    private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = powerStrings.NAME;
+    public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+
+    public NegativelyChargedPower(AbstractCreature owner) {
+        this.name = NAME;
+        this.ID = POWER_ID;
+        this.owner = owner;
+        updateDescription();
+        this.img = TextureLoader.getTexture("MisakaResources/images/powers/Negative.png");
+        this.canGoNegative = false;
+    }
+}
