3dd0e4b3b91acf6e0565baa4da296076bda5cfa5
ReinaSHSL <ReinaSHSL@users.noreply.github.com>
2019-01-12 18:20:15 -0800

    Add character

--- src/main/java/MisakaCode/MisakaModInitializer.java
+++ src/main/java/MisakaCode/MisakaModInitializer.java
@@ -1,17 +1,22 @@
 package MisakaCode;
 
+import MisakaCode.character.MisakaMikoto;
 import MisakaCode.patches.AbstractCardEnum;
+import MisakaCode.patches.MisakaMikotoEnum;
 import basemod.BaseMod;
+import basemod.interfaces.EditCharactersSubscriber;
 import basemod.interfaces.EditStringsSubscriber;
 import com.badlogic.gdx.graphics.Color;
 import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.helpers.CardHelper;
 import com.megacrit.cardcrawl.localization.*;
 
 @SpireInitializer
 public class MisakaModInitializer implements
-        EditStringsSubscriber {
+        EditStringsSubscriber,
+        EditCharactersSubscriber {
     public static final Color MAGNETIC = CardHelper.getColor(255.0f, 255.0f, 255.0f);
     private static final String ATTACK_MAGNETIC = "MisakaResources/images/512/attack_silver.png";
     private static final String SKILL_MAGNETIC = "MisakaResources/images/512/skill_silver.png";
@@ -22,6 +27,8 @@ public class MisakaModInitializer implements
     private static final String SKILL_MAGNETIC_1024 = "MisakaResources/images/1024/skill_silver.png";
     private static final String POWER_MAGNETIC_1024 = "MisakaResources/images/1024/power_silver.png";
     private static final String ENERGY_ORB_GREY_1024 = "MisakaResources/images/1024/energy_orb_magnetic.png";
+
+    private static CharacterStrings characterStrings = CardCrawlGame.languagePack.getCharacterString("MisakaMikoto");
 
     public MisakaModInitializer() {
         BaseMod.subscribe(this);
@@ -43,4 +50,10 @@ public class MisakaModInitializer implements
         }
     }
 
-}
+        @Override
+        public void receiveEditCharacters() {
+            BaseMod.addCharacter(new MisakaMikoto(characterStrings.NAMES[0]),
+                    "MisakaResources/images/character/button.png", "MisakaResources/images/character/portrait.png",
+                    MisakaMikotoEnum.THE_RAILGUN);
+        }
+    }
