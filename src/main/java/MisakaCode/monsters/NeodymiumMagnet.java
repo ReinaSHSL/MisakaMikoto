71ed254e85bd10081706ee234d32aea7866a7411
ReinaSHSL <ReinaSHSL@users.noreply.github.com>
2019-01-13 14:18:29 -0800

    Neodymium magnet

--- src/main/java/MisakaCode/monsters/NeodymiumMagnet.java
+++ src/main/java/MisakaCode/monsters/NeodymiumMagnet.java
@@ -7,12 +7,15 @@ public class NeodymiumMagnet extends AbstractMisakaMonster {
     public static final String ID = "Mikasa:NeodymiumMagnet";
     private static final MonsterStrings monsterStrings = CardCrawlGame.languagePack.getMonsterStrings(ID);
     private static final String NAME = monsterStrings.NAME;
+    private static final int MAX_HP = 5;
     private static final float HB_X = -2.0F;
     private static final float HB_Y = 10.0F;
+    private static final float HB_W = 230.0F;
+    private static final float HB_H = 240.0F;
+    private static final String IMG = "MisakaResources/images/monsters/NeodymiumMagnet.png";
 
-
-    public NeodymiumMagnet() {
-        super(name, id, maxHealth, hb_x, hb_y, hb_w, hb_h, imgUrl, offsetX, offsetY, ignoreBlights);
+    public NeodymiumMagnet(float offsetX) {
+        super(NAME, ID, MAX_HP, HB_X, HB_Y, HB_W, HB_H, IMG, offsetX, 0, true);
     }
 
     @Override
