698ca0a3be158dfe7b3b1bffaa7bf2faa9b3b8fa
ReinaSHSL <ReinaSHSL@users.noreply.github.com>
2019-01-12 18:41:18 -0800

    Base magic number

--- src/main/java/MisakaCode/cards/AbstractMisakaCard.java
+++ src/main/java/MisakaCode/cards/AbstractMisakaCard.java
@@ -4,6 +4,7 @@ import basemod.abstracts.CustomCard;
 
 public abstract class AbstractMisakaCard extends CustomCard {
     public int misakaMagicNumber;
+    public int baseMisakaMagicNumber;
     public boolean misakaMagicNumberChanged = false;
 
     public AbstractMisakaCard(String id, String name, String img, int cost, String rawDescription, CardType type, CardColor color, CardRarity rarity, CardTarget target) {
@@ -11,8 +12,7 @@ public abstract class AbstractMisakaCard extends CustomCard {
     }
 
     public void upgradeMisakaMagicNumber(int i) {
-        this.misakaMagicNumber += i;
+        this.baseMisakaMagicNumber += i;
         this.misakaMagicNumberChanged = true;
     }
-
 }
