c86d7972e3561c5fb3a0e679c370e964b56e32f7
ReinaSHSL <ReinaSHSL@users.noreply.github.com>
2019-01-12 18:21:21 -0800

    Add card tags

---
+++ src/main/java/MisakaCode/patches/MisakaCardTags.java
@@ -0,0 +1,11 @@
+package MisakaCode.patches;
+
+import com.evacipated.cardcrawl.modthespire.lib.SpireEnum;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+
+public class MisakaCardTags {
+    @SpireEnum
+    public static AbstractCard.CardTags isPositive;
+    @SpireEnum
+    public static AbstractCard.CardTags isNegative;
+}
