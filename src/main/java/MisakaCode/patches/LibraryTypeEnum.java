e0a97987a9b46b27906a2b879e8615f6150e3698
ReinaSHSL <ReinaSHSL@users.noreply.github.com>
2019-01-12 17:39:23 -0800

    Library type enum

---
+++ src/main/java/MisakaCode/patches/LibraryTypeEnum.java
@@ -0,0 +1,9 @@
+package MisakaCode.patches;
+
+import com.evacipated.cardcrawl.modthespire.lib.SpireEnum;
+import com.megacrit.cardcrawl.helpers.CardLibrary;
+
+public class LibraryTypeEnum {
+    @SpireEnum
+    CardLibrary.LibraryType MISAKA_MAGNETIC;
+}
