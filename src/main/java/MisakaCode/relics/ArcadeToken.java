3b0db15f4bae82032a2a0b6891f2b7445d9cf84d
ReinaSHSL <ReinaSHSL@users.noreply.github.com>
2019-01-14 04:52:10 -0800

    Arcade Token

--- src/main/java/MisakaCode/relics/ArcadeToken.java
+++ src/main/java/MisakaCode/relics/ArcadeToken.java
@@ -2,13 +2,19 @@ package MisakaCode.relics;
 
 import MisakaCode.tools.TextureLoader;
 import com.badlogic.gdx.graphics.Texture;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
+import com.megacrit.cardcrawl.actions.common.RelicAboveCreatureAction;
 import com.megacrit.cardcrawl.actions.utility.UseCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.relics.AbstractRelic;
 
 public class ArcadeToken extends AbstractMisakaRelic {
     public static final String ID = "misaka:ArcadeToken";
     private static final Texture IMG = TextureLoader.getTexture("MisakaResources/resources/relics/ArcadeToken.png");
+    private static final int d = 5;
 
     public ArcadeToken() {
         super(ID, IMG, RelicTier.STARTER, LandingSound.CLINK);
@@ -16,12 +22,18 @@ public class ArcadeToken extends AbstractMisakaRelic {
 
     @Override
     public String getUpdatedDescription() {
-        return DESCRIPTIONS[0];
+        return DESCRIPTIONS[0] + d + DESCRIPTIONS[1];
     }
 
     @Override
     public void onUseCard(AbstractCard c, UseCardAction a) {
-
+        if(this.counter < 0) {
+            this.counter = 0;
+        }
+        this.counter++;
+        if (this.counter >= 8) {
+            act(new RelicAboveCreatureAction(AbstractDungeon.player, this));
+            act(new DamageAllEnemiesAction(null, DamageInfo.createDamageMatrix(d, true), DamageInfo.DamageType.THORNS, AbstractGameAction.AttackEffect.SLASH_HEAVY));
+        }
     }
-
 }
