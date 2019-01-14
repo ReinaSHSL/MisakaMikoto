23336f5d984d2f920fb484ff1c224fbe851e4ee4
ReinaSHSL <ReinaSHSL@users.noreply.github.com>
2019-01-14 00:24:16 -0800

    throw shade at megacrit

--- src/main/java/MisakaCode/actions/MisakaSpawnMonsterAction.java
+++ src/main/java/MisakaCode/actions/MisakaSpawnMonsterAction.java
@@ -5,10 +5,13 @@ import MisakaCode.monsters.AbstractMisakaMonster;
 import MisakaCode.monsters.NeodymiumMagnet;
 import MisakaCode.powers.PositivelyChargedPower;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.animations.TalkAction;
 import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.SpawnMonsterAction;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.core.Settings;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.MonsterStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.monsters.exordium.ApologySlime;
 
@@ -16,7 +19,6 @@ public class MisakaSpawnMonsterAction extends AbstractMisakaAction {
     private String m;
     private AbstractMonster target;
     private float shoveAmount = 50.0F * Settings.scale;
-
     public MisakaSpawnMonsterAction(String m, AbstractMonster target) {
         this.m = m;
         this.target = target;
@@ -40,7 +42,9 @@ public class MisakaSpawnMonsterAction extends AbstractMisakaAction {
                     act(new ApplyPowerAction(neodymiumMagnet, neodymiumMagnet, new PositivelyChargedPower(neodymiumMagnet)));
                     tickDuration();
                 default:
-                    act(new SpawnMonsterAction(new ApologySlime(), false));
+                    AbstractMonster slimeyboi = new ApologySlime();
+                    act(new SpawnMonsterAction(slimeyboi, false));
+                    act(new TalkAction(slimeyboi, "Aw, something went wrong... NL please let the devs know! NL Also yell at MegaCrit for me being a hardcoded mess.", 4.0F, 4.0F));
             }
         }
     }
