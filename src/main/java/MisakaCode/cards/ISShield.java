698ca75e9861a04e04a82b338fa98ffeb9a35cd4
ReinaSHSL <ReinaSHSL@users.noreply.github.com>
2019-01-16 02:30:27 -0800

    -this.

--- src/main/java/MisakaCode/cards/ISShield.java
+++ src/main/java/MisakaCode/cards/ISShield.java
@@ -4,6 +4,7 @@ import MisakaCode.patches.AbstractCardEnum;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.CardStrings;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
