94e02b08386cc643fb1ff51a840bebd10b3d8a11
ReinaSHSL <ReinaSHSL@users.noreply.github.com>
2019-01-16 19:33:24 -0800

    import

--- src/main/java/MisakaCode/patches/FuckYouMegacritWhyIsntThisFixedYetPatch.java
+++ src/main/java/MisakaCode/patches/FuckYouMegacritWhyIsntThisFixedYetPatch.java
@@ -3,6 +3,7 @@ package MisakaCode.patches;
 import basemod.patches.com.megacrit.cardcrawl.core.CardCrawlGame.CustomRewardLoad;
 import com.evacipated.cardcrawl.modthespire.lib.*;
 import com.megacrit.cardcrawl.actions.common.SpawnMonsterAction;
+import com.megacrit.cardcrawl.actions.unique.CanLoseAction;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.monsters.exordium.AcidSlime_L;
@@ -21,7 +22,7 @@ public class FuckYouMegacritWhyIsntThisFixedYetPatch {
     public static SpireReturn Insert(SlimeBoss __instance) {
         AbstractDungeon.actionManager.addToBottom(new SpawnMonsterAction(new SpikeSlime_L(120.0F, 20.0F, 0, __instance.currentHealth), false));
         AbstractDungeon.actionManager.addToBottom(new SpawnMonsterAction(new AcidSlime_L(-385.0F, -8.0F, 0, __instance.currentHealth), false));
-        AbstractDungeon.actionManager.addToBottom(new com.megacrit.cardcrawl.actions.unique.CanLoseAction());
+        AbstractDungeon.actionManager.addToBottom(new CanLoseAction());
         __instance.setMove(SlimeBoss.MOVES[2], (byte)3, AbstractMonster.Intent.UNKNOWN);
         return SpireReturn.Return(null);
     }
