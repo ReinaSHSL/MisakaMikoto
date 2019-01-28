2a88940360771c324ba940889a8cd980110ee1ad
ReinaSHSL <ReinaSHSL@users.noreply.github.com>
2019-01-28 04:03:42 -0800

    make pp large later

--- src/main/java/MisakaCode/cards/AbstractMisakaCard.java
+++ src/main/java/MisakaCode/cards/AbstractMisakaCard.java
@@ -30,10 +30,13 @@ public abstract class AbstractMisakaCard extends CustomCard {
     public int misakaMagicNumber;
     public int baseMisakaMagicNumber;
     public boolean misakaMagicNumberChanged = false;
-    public CardGroup pp = ProgramCardsPatch.ProgramPileField.programPile.get(AbstractDungeon.player);
+    public CardGroup pp;
 
     public AbstractMisakaCard(String id, String name, String img, int cost, String rawDescription, CardType type, CardColor color, CardRarity rarity, CardTarget target) {
         super(id, name, null, cost, rawDescription, type, color, rarity, target);
+        if (AbstractDungeon.player != null) {
+            pp = ProgramCardsPatch.ProgramPileField.programPile.get(AbstractDungeon.player);
+        }
 
     }
 
