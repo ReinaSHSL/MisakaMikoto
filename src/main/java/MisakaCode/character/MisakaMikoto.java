e612cb8bfe98c167678056e431b0ed326519b3d8
ReinaSHSL <ReinaSHSL@users.noreply.github.com>
2019-01-28 11:29:38 -0800

    this card draw wtf

--- src/main/java/MisakaCode/character/MisakaMikoto.java
+++ src/main/java/MisakaCode/character/MisakaMikoto.java
@@ -44,7 +44,7 @@ public class MisakaMikoto extends CustomPlayer {
     public static final int STARTING_HP = 75;
     public static final int MAX_HP = 75;
     public static final int STARTING_GOLD = 99;
-    public static final int CARD_DRAW = 9;
+    public static final int CARD_DRAW = 5;
     public static final int ORB_SLOTS = 0;
     private static CharacterStrings characterStrings = CardCrawlGame.languagePack.getCharacterString("MisakaMikoto");
 
