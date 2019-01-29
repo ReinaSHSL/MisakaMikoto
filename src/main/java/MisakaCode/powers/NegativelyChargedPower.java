10dcd17cb68024d429c246ce4e2234b9a8e1ec3d
ReinaSHSL <ReinaSHSL@users.noreply.github.com>
2019-01-29 03:37:09 -0800

    new attrac/repel logic for negative

--- src/main/java/MisakaCode/powers/NegativelyChargedPower.java
+++ src/main/java/MisakaCode/powers/NegativelyChargedPower.java
@@ -5,6 +5,7 @@ import MisakaCode.actions.RepelAction;
 import MisakaCode.tools.TextureLoader;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
@@ -34,6 +35,19 @@ public class NegativelyChargedPower extends AbstractMisakaPower {
         if (owner.hasPower(PositivelyChargedPower.POWER_ID)) {
             act(new RemoveSpecificPowerAction(owner, owner, PositivelyChargedPower.POWER_ID));
         }
+    }
+
+    @Override
+    public float atDamageReceive(float d, DamageInfo.DamageType t) {
+        for (AbstractMonster m : aq()) {
+            if (m.hasPower(PositivelyChargedPower.POWER_ID)) {
+                act(na(m, nd((int)d, true)));
+            }
+        }
+        if (AbstractDungeon.player.hasPower(NegativelyChargedPower.POWER_ID)) {
+            act(nz((int)d));
+        }
+        return d;
     }
 
     @Override
